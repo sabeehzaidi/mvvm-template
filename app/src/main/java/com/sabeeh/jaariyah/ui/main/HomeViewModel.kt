be0e43724d7940cc7d0584fb828d4f2362e22340
sabeehzaidi <sabeehz@gmail.com>
2023-12-06 22:58:49 -0500

    Home Fragment and ViewModel added

---
+++ app/src/main/java/com/sabeeh/jaariyah/ui/main/HomeViewModel.kt
@@ -0,0 +1,10 @@
+package com.sabeeh.jaariyah.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    fun getProperty() {
+
+    }
+}
--- app/src/main/java/com/sabeeh/jaariyah/ui/main/MainActivity.kt
+++ app/src/main/java/com/sabeeh/jaariyah/ui/main/MainActivity.kt
@@ -5,8 +5,15 @@ import android.os.Bundle
 import com.sabeeh.jaariyah.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, HomeFragment())
+                .commit()
+        }
     }
 }
