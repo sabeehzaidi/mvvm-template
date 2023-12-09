fde58fc99c008b1373d42e6cfc080e0efb945c5c
sabeehzaidi <sabeehz@gmail.com>
2023-12-08 21:05:04 -0500

    Added navigation controller

--- app/src/main/java/com/sabeeh/jaariyah/ui/main/HomeFragment.kt
+++ app/src/main/java/com/sabeeh/jaariyah/ui/main/HomeFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.sabeeh.jaariyah.R
 import com.sabeeh.jaariyah.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
-    private lateinit var viewModel : HomeViewModel
+    private val viewModel: HomeViewModel by viewModels()
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
@@ -23,7 +27,11 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
-        binding.title.text = "OnViewCreated"
+        viewModel.setTitle(binding.title, "onViewCreated")
+        navController = Navigation.findNavController(view)
+
+        binding.title.setOnClickListener {
+//            navController.navigate(R.id.action_homeFragment_to_placeholder)
+        }
     }
 }
--- app/src/main/java/com/sabeeh/jaariyah/ui/main/HomeViewModel.kt
+++ app/src/main/java/com/sabeeh/jaariyah/ui/main/HomeViewModel.kt
@@ -1,7 +1,11 @@
 package com.sabeeh.jaariyah.ui.main
 
+import android.widget.TextView
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
+    fun setTitle(textView: TextView, text: String) {
+        textView.text = text
+    }
 }
